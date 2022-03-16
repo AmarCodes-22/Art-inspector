28f8963362ac3d0de5eeebeb25b564bb224b970f
Amar <amarpratap2208@gmail.com>
2022-03-16 19:20:05 +0530

    Implement forward loop

--- scripts/train_baseline.py
+++ scripts/train_baseline.py
@@ -1,10 +1,13 @@
+
+#todo: implement training loop with Wrapper loss function over CrossEntropy
+from pprint import pprint
+
 from src.data.cifar100 import Cifar100
 from src.models.baseline import Baseline
 
 
 def load_cifar100():
     cifar100 = Cifar100()
-    # todo: use load_branch_dataloaders here
     dataloaders = cifar100.load_branch_dataloaders()
 
     return dataloaders
@@ -32,4 +35,22 @@ if __name__ == "__main__":
     cifar_branch_dataloaders = load_cifar100()
 
     baseline = load_baseline(branch_dataloaders=cifar_branch_dataloaders)
-    baseline.print_model_summary(input_size=(16, 3, 32, 32))
+    # baseline.print_model_summary(input_size=(16, 3, 32, 32))
+
+    for branch_name, branch_dataloaders in cifar_branch_dataloaders.items():
+        print(f'Branch: {branch_name}')
+        # pprint(branch_dataloaders)
+
+        train_features, train_labels = next(iter(branch_dataloaders["train"]))
+        print(f"Features shape: {train_features.shape}")
+        print(f"Labels shape: {train_labels.shape}")
+
+        # output with branch_name
+        out = baseline(train_features, branch_name)
+        print('Output shape when passing through single branch', out.shape)
+
+        # output without branch_name
+        out = baseline(train_features)
+        print('Output shape when passing through all branches (inference time)', out.shape)
+
+        break
