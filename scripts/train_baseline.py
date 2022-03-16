4b6cc60f1881aa63050a54493eab60df280a6a78
Amar <amarpratap2208@gmail.com>
2022-03-16 18:17:19 +0530

    Use dataloaders instead of datasets

--- scripts/train_baseline.py
+++ scripts/train_baseline.py
@@ -1,35 +1,35 @@
-from pprint import pprint
-from typing import Optional
-
 from src.data.cifar100 import Cifar100
 from src.models.baseline import Baseline
 
-# Instantiate dataset
+
 def load_cifar100():
     cifar100 = Cifar100()
-    datasets = cifar100.load_branch_datasets()
+    # todo: use load_branch_dataloaders here
+    dataloaders = cifar100.load_branch_dataloaders()
 
-    return datasets
+    return dataloaders
 
 
-# Instantiate Baseline
-def load_baseline(branch_datasets: dict):
+def load_baseline(branch_dataloaders: dict):
     resnet_layers = [1, 1, 1, 1]
     model = Baseline(stem_layers=resnet_layers)
 
     stem_out_size = model.stem.get_output_size(input_size=(16, 3, 32, 32))
 
-    branches = {}
-    for k, v in branch_datasets.items():
-        branches[k] = tuple((stem_out_size, len(v["train"].classes)))
+    # make branch_configs used to initialize branches
+    branch_configs = {}
+    for branch_name, branch_dataloader in branch_dataloaders.items():
+        branch_configs[branch_name] = tuple(
+            (stem_out_size, len(branch_dataloader["train"].dataset.classes))
+        )
 
-    model.initialize_branches(branches=branches)
+    model.initialize_branches(branch_configs=branch_configs)
     return model
 
 
 # todo: use argparse here
 if __name__ == "__main__":
-    cifar_branch_datasets = load_cifar100()
+    cifar_branch_dataloaders = load_cifar100()
 
-    baseline = load_baseline(branch_datasets=cifar_branch_datasets)
+    baseline = load_baseline(branch_dataloaders=cifar_branch_dataloaders)
     baseline.print_model_summary(input_size=(16, 3, 32, 32))
