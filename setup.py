316a8de19e3177daf5a7ab37a4e5fec91b3eb751
Amar <amarpratap2208@gmail.com>
2022-03-21 14:10:24 +0530

    Make repo installable as package

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+
+import pkg_resources
+from setuptools import setup, find_packages
+
+setup(
+    name='art_inspector',
+    py_modules=['src'],
+    version='1.0',
+    description='',
+    author='Amar',
+    install_requires=[
+        str(r) for r in pkg_resources.parse_requirements(
+            open(os.path.join(os.path.dirname(__file__), 'requirements_dev.txt'))
+        )
+    ]
+)
