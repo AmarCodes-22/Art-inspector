3fa4e23bff903592931e4a455ff7f9898daedf5d
Amar <amarpratap2208@gmail.com>
2022-03-21 23:07:03 +0530

    Add packages to setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     version='1.0',
     description='',
     author='Amar',
+    packages=find_packages()
     install_requires=[
         str(r) for r in pkg_resources.parse_requirements(
             open(os.path.join(os.path.dirname(__file__), 'requirements_dev.txt'))
